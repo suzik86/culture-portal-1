6c6a018bc70451a1629654ba4c3d7d2689d8278a
mbulldozer <mbulldozer@mail.ru>
2019-07-25 00:55:47 +0300

    init: start codejam-culture-portal

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "import/extensions": "off",
+    "react/jsx-filename-extension": [0],
+    "react/prefer-stateless-function": "off",
+    "react/prop-types": "off",
+  },
+};

fb64da5d49d75e58a05e5848aa04e5056ff92484
enerjaizzzer <enerjaizzzer@gmail.com>
2019-07-27 17:11:42 +0300

    feat: add architector name

--- src/components/architector/architector.js
+++ src/components/architector/architector.js
@@ -5,21 +5,36 @@ import Col from 'react-bootstrap/Col';
 
 
 import PhotoArchitector from './componentParts/photoArchitector';
+import NameArchitector from './componentParts/nameArchitector';
+// import YearsLifeArchitector from './componentParts/yearsLifeArchitector';
+// import LocationArchitector from './componentParts/locationArchitector';
+// import BiographyArchitector from './componentParts/biographyArchitecto';
+// import NameArchitector from './componentParts/nameArchitector';
+// import NameArchitector from './componentParts/nameArchitector';
+// import NameArchitector from './componentParts/nameArchitector';
+// import NameArchitector from './componentParts/nameArchitector';
 
 class Architector extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photo: `https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg`,
-      name: 'this.props.name',
-      date: 'this.props.date',
-      location: 'this.props.location',
-      description: 'this.props.description'
+      photo: this.props.photo || `https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg`,
+      name: this.props.name || `Иванов Иван Иванович`,
+      date: {
+        years: `Годы жизни: `,
+        life: this.props.life || `14 января 1918 - 23 сентября 1997`
+      },
+      location: {
+        place: `Место рождения: `,
+        placeInfo: this.props.place || `г.Алатырь, Симбирская губерния (ныне Чувашия)`
+      },
+      biography: {
+        title: `Биография`,
+        description: this.props.description || `советский архитектор и педагог, работавший в конце 1940-х — начале 1950-х годов в Литве; заслуженный архитектор Беларуси (1980), член-корреспондент Белорусской академии архитектуры.`
+      }
+
     };
   }
-
-
-
 
   render() {
 
@@ -27,14 +42,11 @@ class Architector extends Component {
       <Container className='container'>
         <Row className='architect-description row'>
           <Col className='col-md text-center'>
-
             <PhotoArchitector url={this.state.photo} />
-            <h2>{this.state.name}</h2>
-            <h2>{this.state.date}</h2>
-            <h2>{this.state.location}</h2>
-            <h2>{this.state.description}</h2>
-            <h2>ololo</h2>
-
+            <NameArchitector name={this.state.name} />
+            {/* <YearsLifeArchitector date={this.state.date} />
+            <LocationArchitector location={this.state.location} />
+            <BiographyArchitector biography={this.state.biography} /> */}
           </Col>
         </Row>
       </Container>
---
+++ src/components/architector/componentParts/yearsLifeArchitector.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function YearsLifeArchitector(props) {
+  const { years, life } = props.date;
+
+  return (
+    <p>
+      <span>{years}</span>
+      <span>{life}</span>
+    </p>
+  )
+}
+
+export default YearsLifeArchitector;

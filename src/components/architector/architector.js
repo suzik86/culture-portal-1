fc1d31f0880f2dbe4d96dd4ae46def463a1a7940
enerjaizzzer <enerjaizzzer@gmail.com>
2019-07-27 15:43:53 +0300

    feat: add architector photo

--- src/components/architector/architector.js
+++ src/components/architector/architector.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-class Architector extends React.Component {
+
+import PhotoArchitector from './componentParts/photoArchitector';
+
+class Architector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photo: `https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg`,
+      name: 'this.props.name',
+      date: 'this.props.date',
+      location: 'this.props.location',
+      description: 'this.props.description'
+    };
+  }
+
+
+
 
   render() {
 
-    const photo = 'photo';
-    const name = 'name';
-    const date = 'date';
-    const location = 'location';
-    const description = 'description';
-
-
-
     return (
-      <div>
-        <h1>{photo}</h1>
-        <h1>{name}</h1>
-        <h1>{date}</h1>
-        <h1>{location}</h1>
-        <h1>{description}</h1>
-        <h1>ololo</h1>
-      </div>
+      <Container className='container'>
+        <Row className='architect-description row'>
+          <Col className='col-md text-center'>
+
+            <PhotoArchitector url={this.state.photo} />
+            <h2>{this.state.name}</h2>
+            <h2>{this.state.date}</h2>
+            <h2>{this.state.location}</h2>
+            <h2>{this.state.description}</h2>
+            <h2>ololo</h2>
+
+          </Col>
+        </Row>
+      </Container>
 
     )
   }
---
+++ src/components/architector/componentParts/photoArchitector.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
+
+function PhotoArchitector(props) {
+  const {url} = props;
+  return (
+    <Image src={url} rounded />
+  )
+}
+
+export default PhotoArchitector;

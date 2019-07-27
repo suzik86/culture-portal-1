26376776e4c0ac698fa75002a1440fda77b6f6b0
enerjaizzzer <enerjaizzzer@gmail.com>
2019-07-27 17:12:52 +0300

    feat: add architector location

--- src/components/architector/architector.js
+++ src/components/architector/architector.js
@@ -7,7 +7,7 @@ import Col from 'react-bootstrap/Col';
 import PhotoArchitector from './componentParts/photoArchitector';
 import NameArchitector from './componentParts/nameArchitector';
 import YearsLifeArchitector from './componentParts/yearsLifeArchitector';
-// import LocationArchitector from './componentParts/locationArchitector';
+import LocationArchitector from './componentParts/locationArchitector';
 // import BiographyArchitector from './componentParts/biographyArchitecto';
 // import NameArchitector from './componentParts/nameArchitector';
 // import NameArchitector from './componentParts/nameArchitector';
@@ -45,8 +45,8 @@ class Architector extends Component {
             <PhotoArchitector url={this.state.photo} />
             <NameArchitector name={this.state.name} />
             <YearsLifeArchitector date={this.state.date} />
-            {/* <LocationArchitector location={this.state.location} />
-            <BiographyArchitector biography={this.state.biography} /> */}
+            <LocationArchitector location={this.state.location} />
+            {/* <BiographyArchitector biography={this.state.biography} /> */}
           </Col>
         </Row>
       </Container>

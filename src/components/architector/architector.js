42209b46288104e77b57162f353b81beaad6f4bf
enerjaizzzer <enerjaizzzer@gmail.com>
2019-07-27 14:13:04 +0300

    init: start component Architector

---
+++ src/components/architector/architector.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Architector extends React.Component {
+
+  render() {
+
+    const photo = 'photo';
+    const name = 'name';
+    const date = 'date';
+    const location = 'location';
+    const description = 'description';
+
+
+
+    return (
+      <div>
+        <h1>{photo}</h1>
+        <h1>{name}</h1>
+        <h1>{date}</h1>
+        <h1>{location}</h1>
+        <h1>{description}</h1>
+        <h1>ololo</h1>
+      </div>
+
+    )
+  }
+}
+
+export default Architector;

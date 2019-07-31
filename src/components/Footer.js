34051bdcbe4ffa1ec35156b42abcde24007be400
mbulldozer <mbulldozer@mail.ru>
2019-07-31 20:45:33 +0300

    feat: add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <span className="footer_corypight">copyright © Rolling Scopes School 2019</span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;

5b5b64f1e3244744589c63b82d8ec1eb68eb0bae
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-01 19:02:51 +0300

    fix: api key

--- src/components/architector/Architector.js
+++ src/components/architector/Architector.js
@@ -41,7 +41,7 @@ class Architector extends Component {
             <VideoCustom />
             <GoogleMapCustom
               coords={this.coords}
-              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlenTY9JOxetvVWBrCyB7CHM0mJzp1YCQ&v=3.exp"
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp"
               loadingElement={<div style={{ height: '100%' }} />}
               containerElement={<div style={{ height: '400px' }} />}
               mapElement={<div style={{ height: '100%' }} />}

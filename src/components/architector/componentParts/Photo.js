93723a7e377c7da6f452efe262e55391bc08fddb
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-02 22:11:13 +0300

    refactor: photo dev

--- src/components/architector/componentParts/Photo.js
+++ src/components/architector/componentParts/Photo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'react-bootstrap/Image';
 
 const Photo = ({ url }) => (
-  <Image src={url} width="350" rounded />
+  <Image className="photo-architector" src={url} width="350" rounded />
 );
 
 export default Photo;

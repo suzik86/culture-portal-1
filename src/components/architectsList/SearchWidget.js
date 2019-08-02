048d3b744b51e9e22888e14b3ad936c21ef9ed02
GYegor <yahor2011@gmail.com>
2019-08-02 20:06:14 +0300

    feat: add searchWidget

--- src/components/architectsList/SearchWidget.js
+++ src/components/architectsList/SearchWidget.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 
+const SearchWidget = ({ onChange, placeholder }) => (
+  <Row>
+    <input className="form-control py-2 border" width="500" id="search" type="text" placeholder={placeholder} onChange={e => onChange(e.target.value)} />
+  </Row>
+);
 
-class SearchWidget extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.textInputHandler = this.textInputHandler.bind(this);
-  // }
-
-  inputHandler(e) {
-    const { onChange } = this.props;
-    onChange(e.target.value);
-  }
-
-  render() {
-    const { placeholder } = this.props;
-    return (
-      <Row>
-        <input className="form-control py-2 border" width="500" id="search" type="text" placeholder={placeholder} value="" onChange={this.inputHandler} />
-      </Row>
-    );
-  }
-}
 export default SearchWidget;

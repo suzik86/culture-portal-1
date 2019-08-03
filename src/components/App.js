5ef3b1eede2f034d1104f891b36939abf588dd08
mbulldozer <mbulldozer@mail.ru>
2019-08-03 21:12:53 +0300

    feat: use contentful to load data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
+import { createClient } from 'contentful';
 import Loader from 'react-loader-spinner';
+import i18n from 'i18next';
 import i18nInit from '../i18n/i18n';
-
 
 import '../../node_modules/react-modal-video/css/modal-video.min.css';
 import '../styles/App.css';
@@ -12,6 +13,11 @@ import MainPage from './mainPage/MainPage';
 import ArchitectsList from './architectsList/ArchitectsList';
 import Header from './Header';
 import Footer from './Footer';
+
+const client = createClient({
+  space: '6xi1ban3eonx',
+  accessToken: '5PCNrHtiMF2-N9s9MKg-S7G4UoyDGIZE-gODtjSdECU',
+});
 
 class App extends Component {
   constructor(props) {
@@ -28,9 +34,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    i18nInit();
-    this.setState({
-      loading: false,
+    client.getEntry('EFoacOPsP62cLFTzIr9uG').then((response) => {
+      const { en, ru, by } = response.fields;
+      i18nInit(en, ru, by);
+      this.setState({
+        loading: false,
+      });
     });
   }
 
@@ -48,7 +57,7 @@ class App extends Component {
   }
 
   render() {
-    const { t, i18n } = this.props;
+    const { t } = this.props;
     const architects = t('architects', { returnObjects: true });
     const site = t('site', { returnObjects: true });
     const listLength = architects.length;
